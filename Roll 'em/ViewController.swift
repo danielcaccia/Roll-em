d12dac412d11f21dcee6fa3074c5bbc4f27035be
Daniel Caccia <danielcaccia@Daniels-iMac.home-life.hub>
2021-01-12 23:38:16 +0100

    Create UI and Functionalities

--- Roll 'em/ViewController.swift
+++ Roll 'em/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rollButton.layer.cornerRadius = 10
+        rollButton.clipsToBounds = true
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let dicesArray = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+        
+        leftDice.image = dicesArray.randomElement()
+        rightDice.image = dicesArray.randomElement()
+    }
+    
 }
 
